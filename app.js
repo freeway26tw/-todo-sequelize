49b8d6a6826bbe55863a4381afad63692115b68b
freeway26tw <freeway26tw@gmail.com>
2023-02-24 23:52:27 +0800

    refactor todo route

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ const methodOverride = require('method-override')
 const bcrypt = require('bcryptjs')
 const passport = require('passport')
 
+const routes = require('./routes')
+
 const usePassport = require('./config/passport')
 
 const app = express()
@@ -26,14 +28,7 @@ app.use(session({
 
 usePassport(app)
 
-app.get('/', (req, res) => {
-  return Todo.findAll({
-    raw: true,
-    nest: true
-  })
-    .then((todos) => { return res.render('index', { todos: todos }) })
-    .catch((error) => { return res.status(422).json(error) })
-})
+app.use(routes)
 
 app.get('/users/login', (req, res) => {
   res.render('login')
@@ -44,7 +39,6 @@ app.post('/users/login', passport.authenticate('local', {
   failureRedirect: '/users/login'
 }))
 
-
 app.get('/users/register', (req, res) => {
   res.render('register')
 })
@@ -53,13 +47,6 @@ app.post('/users/register', (req, res) => {
   const { name, email, password, confirmPassword } = req.body
   User.create({ name, email, password })
     .then(user => res.redirect('/'))
-})
-
-app.get('/todos/:id', (req, res) => {
-  const id = req.params.id
-  return Todo.findByPk(id)
-    .then(todo => res.render('detail', { todo: todo.toJSON() }))
-    .catch(error => console.log(error))
 })
 
 app.post('users/register', (req, res) => {
---
+++ routes/modules/todos.js
@@ -0,0 +1,52 @@
+const express = require('express')
+const router = express.Router()
+
+const Todo = require('../../models/todo')
+
+router.get('/new', (req, res) => {
+  return res.render('new')
+})
+
+router.post('/', (req, res) => {
+  const userId = req.user.id
+  const name = req.body.name
+
+  return Todo.create({ name, userId })
+    .then(() => res.redirect('/'))
+    .catch(error => console.log(error))
+})
+
+router.get('/:id', (req, res) => {
+  const id = req.params.id
+  return Todo.findByPk(id)
+    .then(todo => res.render('detail', { todo: todo.toJSON() }))
+    .catch(error => console.log(error))
+})
+
+router.get('/:id/edit', (req, res) => {
+  const id = req.params.id
+  return Todo.findByPk(id)
+    .then(todo => res.render('edit', {
+      todo: todo.toJSON()
+    }))
+    .catch(error => console.log(error))
+})
+
+router.put('/:id', (req, res) => {
+  const id = req.params.id
+  const { name, isDone } = req.body
+
+  return Todo.update({ name, isDone: isDone === 'on' }, { where: { id } })
+    .then(() => res.redirect(`/todos/${id}`))
+    .catch(error => console.log(error))
+})
+
+router.delete('/:id', (req, res) => {
+  const id = req.params.id
+  return Todo.destroy({ where: { id } })
+    .then(() => res.redirect('/'))
+    .catch(error => console.log(error))
+})
+
+
+module.exports = router
