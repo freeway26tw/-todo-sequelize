1831aeeacf7022b6afb9d678938de54f5b2a4f98
freeway26tw <freeway26tw@gmail.com>
2023-02-24 18:03:51 +0800

    login route and local strategy

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
 const express = require('express')
+const session = require('express-session')
 const exphbs = require('express-handlebars')
 const methodOverride = require('method-override')
 const bcrypt = require('bcryptjs')
+const passport = require('passport')
+
+const usePassport = require('./config/passport')
+
 const app = express()
 const PORT = 3000
 
@@ -13,6 +18,13 @@ app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
 app.use(express.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
+app.use(session({
+  secret: 'ThisIsMySecret',
+  resave: false,
+  saveUninitialized: true
+}))
+
+usePassport(app)
 
 app.get('/', (req, res) => {
   return Todo.findAll({
@@ -27,9 +39,11 @@ app.get('/users/login', (req, res) => {
   res.render('login')
 })
 
-app.post('/users/login', (req, res) => {
-  res.send('login')
-})
+app.post('/users/login', passport.authenticate('local', {
+  successRedirect: '/',
+  failureRedirect: '/users/login'
+}))
+
 
 app.get('/users/register', (req, res) => {
   res.render('register')
@@ -49,8 +63,8 @@ app.get('/todos/:id', (req, res) => {
 })
 
 app.post('users/register', (req, res) => {
-  const {name, email, password, confirmPassword} = req.body
-  User.findOne({ where: {email}}).then(user => {
+  const { name, email, password, confirmPassword } = req.body
+  User.findOne({ where: { email } }).then(user => {
     if (user) {
       console.log('User already exists')
       return res.render('register', {
