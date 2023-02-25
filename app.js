9c75b6a5d98fc829951c4bb06f74c9774b329adc
freeway26tw <freeway26tw@gmail.com>
2023-02-25 09:42:58 +0800

    gitigore dotenv

--- app.js
+++ app.js
@@ -5,6 +5,10 @@ const methodOverride = require('method-override')
 const bcrypt = require('bcryptjs')
 const passport = require('passport')
 
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
+
 const routes = require('./routes')
 
 const usePassport = require('./config/passport')
@@ -12,12 +16,9 @@ const usePassport = require('./config/passport')
 const app = express()
 const PORT = 3000
 
-const db = require('./models')
-const Todo = db.Todo
-const User = db.User
-
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
+
 app.use(express.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
 app.use(session({
