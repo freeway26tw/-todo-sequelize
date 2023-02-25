c572ed12a87d8255041bbd8eb42f6e808f9462aa
freeway26tw <freeway26tw@gmail.com>
2023-02-25 10:56:22 +0800

    add facebook login auth route

--- routes/index.js
+++ routes/index.js
@@ -4,9 +4,11 @@ const router = express.Router()
 const home = require('./modules/home')
 const todos = require('./modules/todos')
 const users = require('./modules/users')
+const auth = require('./modules/auth')
 
-router.use('/todos',  todos)
+router.use('/todos', todos)
 router.use('/users', users)
-router.use('/',  home)
+router.use('/auth', auth)
+router.use('/', home)
 
 module.exports = router
